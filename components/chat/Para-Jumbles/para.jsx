5c7bcda5f577a5ff8732bc3f12e16158deba8f93
pradum singh <102753906+PRADUMSINGH01@users.noreply.github.com>
2024-11-14 17:53:08 +0000

    done jumble

--- components/chat/Para-Jumbles/para.jsx
+++ components/chat/Para-Jumbles/para.jsx
@@ -3,16 +3,20 @@ import { useState,useEffect } from 'react';
 import FETCHDATA from '@/module/fetchdata';
 import { FaCaretRight } from "react-icons/fa";
 import { FaCaretLeft } from "react-icons/fa";
+import { PiBoulesFill } from "react-icons/pi";
+import { IoMdClose } from "react-icons/io";
 
 const Para = ({ para }) => {
 
   const [show , setShow] =useState(false)
    const [data, setdata] =useState([])
 const [next , setnext] = useState(0)
-  
+  const [Loading , setLoading] = useState(true)
+  const [alert , setalert ]= useState(false)
 useEffect(()=>{
 async function fetchdata(){
   const userss = await FETCHDATA('/Jumble')
+  setLoading(false)
   return setdata(userss)
 }
 
@@ -22,6 +26,7 @@ async function fetchdata(){
 
   
   const showpara = ()=>{
+    
 if(show){
   setShow(false)
 }else{
@@ -33,8 +38,10 @@ if(show){
 
 
 const Next =()=>{
-  if(next>=0){
+  if(next<data.length){
     setnext(next+1)
+  }else{
+    setalert(true)
   }
   
 }
@@ -47,7 +54,9 @@ const Next =()=>{
   
   return (
     <div>
-
+      
+<div className={`${alert?"flex":"hidden"} absolute top-12 w-full h-16 bg-green-500 text-white  text-center items-center justify-center font-semibold `} > <PiBoulesFill />You have Completed this section  <button onClick={()=>setalert(false)} className='items-left ml-10'> <IoMdClose />
+</button> </div> 
       
       <h1 className="w-full h-14 bg-white shadow-lg border border-b-blue-200 flex justify-center items-center text-2xl fixed ">
       
@@ -57,8 +66,8 @@ const Next =()=>{
     <div className=" w-full h-screen flex flex-col justify-between items-center p-10"> 
 
 
-      {
-        data.slice(next,next+1).map((data)=>(
+{
+  Loading ? <span className=' animate-spin w-10 h-10 border rounded-full border-black mt-20 ' >   </span>:     data.slice(next,next+1).map((data)=>(
     <div className="flex flex-col items-left w-full   justify-between  mt-10 ">
           
       <div className='md:m-5 m-1   text-md md:text-2xl'>
@@ -92,10 +101,10 @@ const Next =()=>{
       }
 
 
-      <div className=' flex flex-wrap justify-between w-full text-white mt-4 '>  
+      <div className=' flex flex-wrap justify-around w-full text-white mt-4 '>  
          <button className=' w-[4rem] flex items-center bg-yellow-500 p-2 rounded-md  shadow-lg ' onClick={Prev} > <FaCaretLeft/> Prev  </button>
-         <button className=' w-[4.3rem] flex items-center bg-red-500 p-2 rounded-md  shadow-lg ' onClick={showpara}> Answer </button>
-      <button className='w-[4rem] flex items-center bg-green-500 p-2 rounded-md  shadow-lg  ' onClick={Next} > Next <FaCaretRight/> </button>
+         <button className=' w-[4.3rem] flex items-center bg-red-500 p-2 rounded-md  shadow-lg ' onClick={showpara}> Answer {next} </button>
+      <button className='w-[4rem] flex items-center bg-green-500 p-2 rounded-md  shadow-lg  '   onClick={Next} > Next <FaCaretRight/> </button>
       </div>
     </div>
     
