9b4c856fce506daad1a6477dd45fc1c38a14e118
iimtarget in <iimtargetin@gmail.com>
2025-01-22 17:11:21 +0000

    fix issue

--- app/Quant/Number-System/Level-two/page.jsx
+++ app/Quant/Number-System/Level-two/page.jsx
@@ -6,40 +6,40 @@ const page = () => {
   const { dataa, loading, error } = usePayment();
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error}</p>;
-  useEffect(() => {
-    const disableRightClick = (event) => {
-      event.preventDefault();
-    };
+  // useEffect(() => {
+  //   const disableRightClick = (event) => {
+  //     event.preventDefault();
+  //   };
 
-    const disableCopy = (event) => {
-      event.preventDefault();
-      alert("Copying is disabled on this website!");
-    };
+  //   const disableCopy = (event) => {
+  //     event.preventDefault();
+  //     alert("Copying is disabled on this website!");
+  //   };
 
-    const disableCut = (event) => {
-      event.preventDefault();
-      alert("Cutting is disabled on this website!");
-    };
+  //   const disableCut = (event) => {
+  //     event.preventDefault();
+  //     alert("Cutting is disabled on this website!");
+  //   };
 
-    const disablePaste = (event) => {
-      event.preventDefault();
-      alert("Pasting is disabled on this website!");
-    };
+  //   const disablePaste = (event) => {
+  //     event.preventDefault();
+  //     alert("Pasting is disabled on this website!");
+  //   };
 
-    // Add event listeners
-    document.addEventListener("contextmenu", disableRightClick);
-    document.addEventListener("copy", disableCopy);
-    document.addEventListener("cut", disableCut);
-    document.addEventListener("paste", disablePaste);
+  //   // Add event listeners
+  //   document.addEventListener("contextmenu", disableRightClick);
+  //   document.addEventListener("copy", disableCopy);
+  //   document.addEventListener("cut", disableCut);
+  //   document.addEventListener("paste", disablePaste);
 
-    // Cleanup the event listeners on component unmount
-    return () => {
-      document.removeEventListener("contextmenu", disableRightClick);
-      document.removeEventListener("copy", disableCopy);
-      document.removeEventListener("cut", disableCut);
-      document.removeEventListener("paste", disablePaste);
-    };
-  }, []);
+  //   // Cleanup the event listeners on component unmount
+  //   return () => {
+  //     document.removeEventListener("contextmenu", disableRightClick);
+  //     document.removeEventListener("copy", disableCopy);
+  //     document.removeEventListener("cut", disableCut);
+  //     document.removeEventListener("paste", disablePaste);
+  //   };
+  // }, []);
 
   return (
     <div>
