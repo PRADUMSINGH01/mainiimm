5ae0a79d565127eaf814bcd3cd8abc5d194fba36
pradum singh <102753906+PRADUMSINGH01@users.noreply.github.com>
2025-02-25 07:25:14 +0000

    pixel

--- app/layout.js
+++ app/layout.js
@@ -35,6 +35,33 @@ export default function RootLayout({ children }) {
         </Script>
       </Head>
       <body className={inter.className}>
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];
+            t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '581904311514964');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+
+        {/* Facebook noscript fallback */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=581904311514964&ev=PageView&noscript=1"
+            alt="Facebook Pixel"
+          />
+        </noscript>
+
         <MathJaxContext config={mathJaxConfig}>
           <SessionProvider>{children}</SessionProvider>
           <script src="https://checkout.razorpay.com/v1/checkout.js"></script>
