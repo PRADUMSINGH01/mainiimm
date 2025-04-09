11299f502a1ffd181c6e695f30c6145048041e65
pradum singh <102753906+PRADUMSINGH01@users.noreply.github.com>
2025-04-09 10:33:13 +0000

    mock done

--- app/FullMock/[...Take]/page.jsx
+++ app/FullMock/[...Take]/page.jsx
@@ -1,9 +1,27 @@
-import Mock_Fetch_Data from '@/module/Mock_Fetch_Data';
-import MockTest from '@/components/chat/mock/mockSection';
+import Mock_Fetch_Data from "@/module/Mock_Fetch_Data";
+import MockTest from "@/components/chat/mock/mockSection";
 
 export async function generateStaticParams() {
   // Define the categories you want to generate paths for
-  const categories = ['Averages', 'algebra', 'Geometry' ,'percentages']; // Add more as needed
+  const categories = [
+    "Averages",
+    "algebra",
+    "Geometry",
+    "percentages",
+    "Number-System",
+    "Interest",
+    "Time-work",
+    "Geometry",
+    "Trigonometry",
+    "Permutation-Combination",
+    "Probability",
+    "Logarithms",
+    "Linear-Equations",
+    "Quadratic-equations",
+    "Functions-and-Graphs",
+    "Inequalities",
+    "Mean-mode-median",
+  ]; // Add more as needed
 
   const paths = [];
 
@@ -24,14 +42,16 @@ const FullMockPage = async ({ params }) => {
 
   // Extract the key part of the route
   const category = Take[0]; // e.g., 'Averages'
-  const id = Take[1];       // e.g., '1'
+  const id = Take[1]; // e.g., '1'
 
   // Fetch data for the specific category
   const data = await Mock_Fetch_Data(`Quant/${category}/Questions`);
   const mock = data.find((item) => item.id.toString() === id);
 
   if (!mock) {
-    return <div className="text-center text-gray-500 mt-10">Mock not found</div>;
+    return (
+      <div className="text-center text-gray-500 mt-10">Mock not found</div>
+    );
   }
 
   return <MockTest Propsdata={mock} />;
